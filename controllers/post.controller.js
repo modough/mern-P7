88b546e435b431ec5f796ce3b7f6e78bf4b584fb
mouhamed <mouhamed.mbaye1@outlook.com>
2022-06-12 13:16:02 +0200

    postController in progress

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,53 +1,54 @@
 const postModel = require("../models/post.model");
 const userModel = require("../models/user.model");
-const objectID = require('mongoose').Types.ObjectId;
+const objectID = require("mongoose").Types.ObjectId;
 
-module.exports.readPost = (req, res) =>{
-    postModel.find((err, docs) => {
-        if (!err) res.send(docs);
-        else console.log('Error to get data:' + err);
-    });
- 
+module.exports.readPost = (req, res) => {
+  postModel.find((err, docs) => {
+    if (!err) res.send(docs);
+    else console.log("Error to get data:" + err);
+  });
 };
 
-module.exports.createPost = async (req, res) =>{
-    const newPost = new postModel({
-        posterId: req.body.posterId,
-        message: req.body.message,
-        likers: [],
-        comments: []
-        
-    });
-    
-    try {
-        const post = await newPost.save();
-        console.log(post)
-        return res.status(201).json(post);   
-    }catch (err) {
-        return res.status(400).send(err);
+module.exports.createPost = async (req, res) => {
+  const newPost = new postModel({
+    posterId: req.body.posterId,
+    message: req.body.message,
+    likers: [],
+    comments: [],
+  });
+
+  try {
+    const post = await newPost.save();
+    console.log(post);
+    return res.status(201).json(post);
+  } catch (err) {
+    return res.status(400).send(err);
+  }
+};
+
+module.exports.updatePost = (req, res) => {
+  if (!objectID.isValid(req.params.id))
+    return res.status(400).send("ID unknown :" + req.params.id);
+
+  const updatedRecord = {
+    message: req.body.message,
+  };
+  postModel.findByIdAndUpdate(
+    req.params.id,
+    { $set: updatedRecord },
+    { new: true },
+    (err, docs) => {
+      if (!err) res.send(docs);
+      else console.log("Update error :" + err);
     }
-
+  );
 };
 
-module.exports.updatePost = (req, res) =>{
-    if (!objectID.isValid(req.params.id))
-    return res.status(400).send('ID unknown :' + req.params.id);
-
-    const updatedRecord = {
-        message: req.body.message
-    }
-    postModel.findByIdAndUpdate(
-        req.params.id,
-        {$set: updatedRecord},
-        {new: true},
-        (err, docs) => {
-            if (!err) res.send(docs);
-            else console.log("Update error:" + err);
-        }
-    )
-
+module.exports.deletePost = (req, res) => {
+  if (!objectID.isValid(req.params.id))
+    return res.status(400).send("ID unknown :" + req.params.id);
+  postModel.findByIdAndRemove(req.params.id, (err, docs) => {
+    if (!err) res.send(docs);
+    else console.log("Delete error :" + err);
+  });
 };
-
-module.exports.deletePost = (req, res) =>{
-
-}
