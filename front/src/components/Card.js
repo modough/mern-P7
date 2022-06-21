f9d78aa58eaa50ba0b162cc70735dbb4f0910170
mouhamed <mouhamed.mbaye1@outlook.com>
2022-06-21 14:50:45 +0200

    updt card

--- front/src/components/Card.js
+++ front/src/components/Card.js
@@ -5,8 +5,12 @@ import LikeButton from "./LikeButton";
 
 const Card = ({ post }) => {
   const [isLoading, setIsLoading] = useState(true);
+  const [isUpdated, setIsUpdated] = useState(true);
+  const [textUpdate, setTextUpdate] = useState(null);
   const usersData = useSelector((state) => state.usersReducer);
- 
+  const userData = useSelector((state) => state.usersReducer);
+
+  const updateItem = () => {};
 
   useEffect(() => {
     usersData !== [0] && setIsLoading(false);
@@ -26,13 +30,33 @@ const Card = ({ post }) => {
               </div>
               <span>{dateParser(post.createdAt)}</span>
             </div>
-            <p>{post.message}</p>
+            {isUpdated === false && <p>{post.message}</p>}
+            {isUpdated && (
+              <div className="update-post">
+                <textarea
+                  defaultValue={post.message}
+                  onchange={(e) => setTextUpdate(e.target.value)}
+                />
+                <button className="btn" onClick={updateItem}>
+                  Valider modification
+                </button>
+              </div>
+            )}
+            ;
             {post.picture && (
               <img src={post.picture} alt="card-pic" className="card-pic" />
             )}
+            ;
+            {userData._id === post.posterId && (
+              <div className="button-container">
+                <div onClick={() => setIsUpdated(!isUpdated)}>
+                  <img src="./img/icons/edit.svg" alt="edit" />
+                </div>
+              </div>
+            )}
             <div className="card-footer">
               <div className="comment"></div>
-              <LikeButton post={post}/>
+              <LikeButton post={post} />
               <img src="./img/icons/share.svg" alt="share" />
             </div>
           </div>
